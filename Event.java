09d1910f9948f0a06d8a84fb09c85967a220a2d8
cyndaquil8 <ivy@mahncke.org>
2021-07-06 17:05:20 -0700

    minor edits - strangers are now randomly chosen

--- Event.java
+++ Event.java
@@ -2,14 +2,11 @@ class Event {
   //identity variables
   String name;
   String desc;
-  boolean metyet;
-  String numid; //numid is defined outside of json file
 
   //mod variables
   int depthmod;
   int sanitymod;
   
-
   //call and response variables
   /*
     variable: opening dialogue
@@ -23,21 +20,15 @@ class Event {
 
   //constructor
   public Event() {
-    /*use name -> derive all vars from matching name in masterlist */
-    metyet = false;
-    numid = null; /*add up all numbers into numid to be parsed later*/
+    
   }
 
 }
   
 class Stranger extends Event {
-  public Stranger(String name) {
-    super();
-  }
   public Stranger() {
     super();
   }
-
 
 }
 
@@ -45,6 +36,5 @@ class Location extends Event {
   public Location(String name) {
     super();
   }
-
 
 }
